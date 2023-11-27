5e067bd19de9ae27c276867ca8c16fd1477202c7
tyeebang <111326879+tyeebang@users.noreply.github.com>
2023-11-27 10:40:43 +0900

    임시

--- shopping_web/WebContent/js/script.js
+++ shopping_web/WebContent/js/script.js
@@ -14,190 +14,39 @@ window.addEventListener("scroll", function () {
     }
 });
 
-
 // 상품 리스트 뽑아오기
-function callItemsAlpha() {
+async function callItemsAlpha() {
+    const RESP = await fetch("../items2.json");
+    const ITEMJSON = await RESP.json();
 
-    let items = [
-        {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }, {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }, {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }
-    ];
-
+    let items = ITEMJSON;
     let itemsHTML = "";
 
     for (let i = 0; i < items.length; i++) {
-        let item = items[i]
-        itemsHTML +=  `
+        let item = items[i];
+        itemsHTML += `
     <div class="item">
-        <a href="#">
-            <img src="${item.img_url}" alt="${item.it_name}" class="it_img">
-            <div class="it_cont">
-                <span class="it_name">${item.it_name}</span>
-                <hr>
-                <span class="it_price">${item.price + 'won'}</span>
-            </div>
-        <a>
+        <img src="${item.img_url}" alt="${item.it_name}" class="it_img">
+        <div class="it_cont">
+            <span class="it_name">${item.it_name}</span>
+            <span class="it_price">${item.price + 'won'}</span>
+        </div>
     </div>
-    `
+    `;
     }
 
-    document.querySelector("#items").innerHTML = '<h1>임시</h1>';
     document.querySelector("#items").innerHTML = itemsHTML;
 }
 
-function callItemsBeta() {
+async function callItemsBeta() {
+    const RESP = await fetch("../items2.json");
+    const ITEMJSON = await RESP.json();
 
-    let ca_items = [
-        {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }, {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }, {
-            "it_name": "와이드 루즈핏 빅사이즈 남성 멜란",
-            "price": 13900,
-            "img_url": "./img/items/aa.jpg"
-        },
-        {
-            "it_name": "오버핏 꽈배기 브이넥 니트",
-            "price": 27900,
-            "img_url": "./img/items/bb.jpg"
-        },
-        {
-            "it_name": "루즈핏 브이넥 박시 조끼 여성의류",
-            "price": 15000,
-            "img_url": "./img/items/cc.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        },
-        {
-            "it_name": "브이넥 울니트 조끼",
-            "price": 36700,
-            "img_url": "./img/items/dd.jpg"
-        }
-    ];
-
+    let ca_items = ITEMJSON;
     let ca_itemsHTML = "";
 
     for (let i = 0; i < ca_items.length; i++) {
-        let ca_item = ca_items[i]
+        let ca_item = ca_items[i];
         ca_itemsHTML +=  `
     <div class="ca_item">
         <a href="#">
@@ -209,10 +58,9 @@ function callItemsBeta() {
             </div>
         <a>
     </div>
-    `
+    `;
     }
 
-    document.querySelector("#ca_items").innerHTML = '<h1>임시</h1>';
     document.querySelector("#ca_items").innerHTML = ca_itemsHTML;
 }
 
